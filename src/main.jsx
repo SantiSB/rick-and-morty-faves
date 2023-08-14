19476e03689009f7feb69ec36bee28cfadc1fa0d
SantiSB <a.santiago.salas.b@gmail.com>
2023-08-14 14:46:07 -0500

    react-and-morty-faves v1.0.0

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import "./styles/index.css"
+import ThemeContext from "./pages/context/ThemeContext.js"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ThemeContext.Provider value={"success"}>
+      <App />
+    </ThemeContext.Provider>
+  </React.StrictMode>
 )
